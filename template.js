67057c93313f1c67c7c3e8525220127b4e1bd1e2
Habibur Rahman <habibur88454@gmail.com>
2024-09-25 23:31:56 +0600

    template string or backtic apply

---
+++ template.js
@@ -0,0 +1,20 @@
+const num1 = 2;
+const num2 = 3;
+const num3 = 8;
+
+const total = 'Total num is: ' + (num1 + num2) + ' and also '+ num3;
+console.log(total);
+const total2 = `Total num is: ${num1 +num2} and also ${num3}`
+console.log(total2);
+
+const letter = 'hey man!!! \n' +
+'how are you \n'+ 
+'you are very good boy'
+console.log(letter)
+
+const letter2 = `
+you are very good boy
+and innocent
+i like it man
+`
+console.log(letter2)
